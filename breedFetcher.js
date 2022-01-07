3e4b567a9a5bb29ce48ea01e9b98c74879f321b3
Wilfred <wilfredemonts@gmail.com>
2022-01-07 00:11:35 -0500

    modularized code, added error and request error handling, and used mocha chai for testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,21 @@
 const request = require('request');
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    
+    if (!data[0]) {
+      callback("Breed not found.", null);
+    } else {
+      callback(null, data[0].description);
+    }
+ 
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  console.log(typeof body);
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-  console.log(data[0].description);
-});
+module.exports = { fetchBreedDescription };
